620c1ec90e6893984747b4667227baf0027ed56b
siraajul <shirajulislamparvez@gmail.com>
2024-10-18 01:12:47 +0600

    Working -> Operators

--- src/A_Java_Basics/D_Operators.java
+++ src/A_Java_Basics/D_Operators.java
@@ -1,4 +1,25 @@
 package A_Java_Basics;
 
 public class D_Operators {
+    public static void main(String[] args) {
+        //Arithmetic
+        int num = 10;
+        int num2 = 20;
+        System.out.println("------------------Arithmetic-----------------------");
+        System.out.println(num + num2);
+        System.out.println(num - num2);
+        System.out.println(num * num2);
+        System.out.println(num2 / num);
+        System.out.println(num2 % num);
+        System.out.println("------------------Relational-----------------------");
+        //Relational
+        int a = 10;
+        int b = 20;
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
 }
